63a341c1191fc8bd93bac75c2b86843683f13a4a
Joshua Carp <jm.carp@gmail.com>
2021-03-26 13:26:38 -0400

    Update datasets.

--- housing-sales/analysis.sql
+++ housing-sales/analysis.sql
@@ -1,3 +1,5 @@
+-- Calculate median sales price by year by residential zoning
+create or replace table whatthecarp.cville_eda_derived.sales_by_year as
 select distinct
   saleyear,
   zoning,
@@ -9,16 +11,15 @@ from (
     details.zoning,
     percentile_cont(saleamount, 0.5) over (partition by extract(year from saledate), details.zoning) as saleamount,
     count(saleamount) over (partition by extract(year from saledate), details.zoning) as count
-  from whatthecarp.cville_eda.real_estate_sales sales
+  from whatthecarp.cville_eda_raw.real_estate_sales sales
   join (
     select
       *,
       rank() over (partition by parcelnumber order by objectid) as rank
-    from whatthecarp.cville_eda.parcel_area_details
+    from whatthecarp.cville_eda_raw.parcel_area_details
   ) details on sales.parcelnumber = details.parcelnumber
   where saleamount != 0
   and rank = 1
 )
 where zoning in ('R-1', 'R-2', 'R-3')
 and saleyear >= 2001
-order by saleyear desc, zoning
--- housing-sales/load.sh
+++ housing-sales/load.sh
@@ -8,5 +8,5 @@ curl -o real-estate-sales.csv https://opendata.arcgis.com/datasets/489adf140c174
 # https://opendata.charlottesville.org/datasets/parcel-area-details
 curl -o parcel-area-details.csv https://opendata.arcgis.com/datasets/0e9946c2a77d4fc6ad16d9968509c588_72.csv
 
-bq load --autodetect --replace whatthecarp:cville_eda.real_estate_sales real-estate-sales.csv
-bq load --autodetect --replace whatthecarp:cville_eda.parcel_area_details parcel-area-details.csv
+bq load --autodetect --replace whatthecarp:cville_eda_raw.real_estate_sales real-estate-sales.csv
+bq load --autodetect --replace whatthecarp:cville_eda_raw.parcel_area_details parcel-area-details.csv
