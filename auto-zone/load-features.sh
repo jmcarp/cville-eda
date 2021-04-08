1717d3eeb04bcef25ae60e6fa1594c34a7c292ac
Joshua Carp <jm.carp@gmail.com>
2021-04-07 22:11:35 -0400

    Use city open data for roads.

--- auto-zone/load-features.sh
+++ auto-zone/load-features.sh
@@ -8,18 +8,12 @@ select
   gpin.gpin,
   cat.distance as cat_distance,
   parks.distance as parks_distance,
-  roads_local.distance as roads_local_distance,
-  roads_primary.distance as roads_primary_distance,
-  roads_secondary.distance as roads_secondary_distance,
-  roads_tertiary.distance as roads_tertiary_distance,
+  roads.distance as roads_distance,
   values.landvaluepersqm,
 from `whatthecarp.cville_eda_derived.geopin` gpin
 left join `whatthecarp.cville_eda_derived.geopin_to_cat` cat on gpin.gpin = cat.gpin
 left join `whatthecarp.cville_eda_derived.geopin_to_park` parks on gpin.gpin = parks.gpin
-left join `whatthecarp.cville_eda_derived.roads_local_distance` roads_local on gpin.gpin = roads_local.gpin
-left join `whatthecarp.cville_eda_derived.roads_primary_distance` roads_primary on gpin.gpin = roads_primary.gpin
-left join `whatthecarp.cville_eda_derived.roads_secondary_distance` roads_secondary on gpin.gpin = roads_secondary.gpin
-left join `whatthecarp.cville_eda_derived.roads_tertiary_distance` roads_tertiary on gpin.gpin = roads_tertiary.gpin
+left join `whatthecarp.cville_eda_derived.geopin_to_roads` roads on gpin.gpin = roads.gpin
 left join `whatthecarp.cville_eda_derived.geopin_to_block` gpin_to_block on gpin.gpin = gpin_to_block.gpin
 left join `whatthecarp.cville_eda_derived.value_by_block` values on gpin_to_block.geoid10 = values.geoid10'
 
--- auto-zone/load-roads.sh
+++ auto-zone/load-roads.sh
@@ -2,119 +2,23 @@
 
 set -euo pipefail
 
-# Load Virginia counties to restrict road distance calculations
-curl -O https://www2.census.gov/geo/tiger/TIGER2010/COUNTY/2010/tl_2010_51_county10.zip
-unzip tl_2010_51_county10.zip -d tl_2010_51_county10
-geojsonify tl_2010_51_county10 counties.csv
-bq load --autodetect --replace whatthecarp:cville_eda_raw.census_counties counties.csv
-
-# http://download.geofabrik.de/north-america/us/virginia.html
-curl -O http://download.geofabrik.de/north-america/us/virginia-latest-free.shp.zip
-unzip virginia-latest-free.shp.zip -d virginia-latest-free.shp 'gis_osm_roads_*'
-
-mkdir -p roads
-geojsonify virginia-latest-free.shp/gis_osm_roads_free_1.shp roads/local.csv 'select *, asgeojson(geometry) as geometry from gis_osm_roads_free_1 where fclass in (''primary'', ''secondary'', ''tertiary'')'
-geojsonify virginia-latest-free.shp/gis_osm_roads_free_1.shp roads/primary.csv 'select *, asgeojson(geometry) as geometry from gis_osm_roads_free_1 where fclass = ''primary'''
-geojsonify virginia-latest-free.shp/gis_osm_roads_free_1.shp roads/secondary.csv 'select *, asgeojson(geometry) as geometry from gis_osm_roads_free_1 where fclass = ''secondary'''
-geojsonify virginia-latest-free.shp/gis_osm_roads_free_1.shp roads/tertiary.csv 'select *, asgeojson(geometry) as geometry from gis_osm_roads_free_1 where fclass = ''tertiary'''
-
-bq load --autodetect --replace whatthecarp:cville_eda_raw.roads_local roads/local.csv
-bq load --autodetect --replace whatthecarp:cville_eda_raw.roads_primary roads/primary.csv
-bq load --autodetect --replace whatthecarp:cville_eda_raw.roads_secondary roads/secondary.csv
-bq load --autodetect --replace whatthecarp:cville_eda_raw.roads_tertiary roads/tertiary.csv
+# https://opendata.charlottesville.org/datasets/road-centerlines
+curl -o road-centerlines.zip https://opendata.arcgis.com/datasets/5ea50546852444a890dc55c9d68104f8_29.zip
+unzip road-centerlines.zip -d road-centerlines
+geojsonify road-centerlines road-centerlines.csv 'select *, asgeojson(geometry) as geometry from road_centerlines where st_class in (''PA'', ''MA'', ''CO'')'
+bq load --autodetect --replace whatthecarp:cville_eda_raw.road_centerlines road-centerlines.csv
 
 bq query --nouse_legacy_sql \
-'create or replace table `whatthecarp.cville_eda_derived.roads_local_distance` as
-with roads as (
-  select
-    roads.*
-  from `whatthecarp.cville_eda_raw.roads_local` roads
-  join `whatthecarp.cville_eda_raw.census_counties` counties
-    on st_intersects(st_geogfromgeojson(roads.geometry), st_geogfromgeojson(counties.geometry))
-  where counties.geoid10 = 51540 -- Charlottesville City
-)
+'create or replace table `whatthecarp.cville_eda_derived.geopin_to_roads` as
 select
   * except (rank)
 from (
   select
     gpin.gpin,
-    roads.osm_id,
-    roads.ref,
+    roads.objectid,
     st_distance(gpin.geometry, st_geogfromgeojson(roads.geometry)) as distance,
     row_number() over (partition by gpin.gpin order by st_distance(gpin.geometry, st_geogfromgeojson(roads.geometry)) asc) as rank
-  from `whatthecarp.cville_eda_derived.geoparcelid` gpin
-  cross join roads
-)
-where rank = 1'
-
-bq query --nouse_legacy_sql \
-'create or replace table `whatthecarp.cville_eda_derived.roads_primary_distance` as
-with roads as (
-  select
-    roads.*
-  from `whatthecarp.cville_eda_raw.roads_primary` roads
-  join `whatthecarp.cville_eda_raw.census_counties` counties
-    on st_intersects(st_geogfromgeojson(roads.geometry), st_geogfromgeojson(counties.geometry))
-  where counties.geoid10 = 51540 -- Charlottesville City
-)
-select
-  * except (rank)
-from (
-  select
-    gpin.gpin,
-    roads.osm_id,
-    roads.ref,
-    st_distance(gpin.geometry, st_geogfromgeojson(roads.geometry)) as distance,
-    row_number() over (partition by gpin.gpin order by st_distance(gpin.geometry, st_geogfromgeojson(roads.geometry)) asc) as rank
-  from `whatthecarp.cville_eda_derived.geoparcelid` gpin
-  cross join roads
-)
-where rank = 1'
-
-bq query --nouse_legacy_sql \
-'create or replace table `whatthecarp.cville_eda_derived.roads_secondary_distance` as
-with roads as (
-  select
-    roads.*
-  from `whatthecarp.cville_eda_raw.roads_secondary` roads
-  join `whatthecarp.cville_eda_raw.census_counties` counties
-    on st_intersects(st_geogfromgeojson(roads.geometry), st_geogfromgeojson(counties.geometry))
-  where counties.geoid10 = 51540 -- Charlottesville City
-)
-select
-  * except (rank)
-from (
-  select
-    gpin.gpin,
-    roads.osm_id,
-    roads.ref,
-    st_distance(gpin.geometry, st_geogfromgeojson(roads.geometry)) as distance,
-    row_number() over (partition by gpin.gpin order by st_distance(gpin.geometry, st_geogfromgeojson(roads.geometry)) asc) as rank
-  from `whatthecarp.cville_eda_derived.geoparcelid` gpin
-  cross join roads
-)
-where rank = 1'
-
-bq query --nouse_legacy_sql \
-'create or replace table `whatthecarp.cville_eda_derived.roads_tertiary_distance` as
-with roads as (
-  select
-    roads.*
-  from `whatthecarp.cville_eda_raw.roads_tertiary` roads
-  join `whatthecarp.cville_eda_raw.census_counties` counties
-    on st_intersects(st_geogfromgeojson(roads.geometry), st_geogfromgeojson(counties.geometry))
-  where counties.geoid10 = 51540 -- Charlottesville City
-)
-select
-  * except (rank)
-from (
-  select
-    gpin.gpin,
-    roads.osm_id,
-    roads.ref,
-    st_distance(gpin.geometry, st_geogfromgeojson(roads.geometry)) as distance,
-    row_number() over (partition by gpin.gpin order by st_distance(gpin.geometry, st_geogfromgeojson(roads.geometry)) asc) as rank
-  from `whatthecarp.cville_eda_derived.geoparcelid` gpin
-  cross join roads
+  from `whatthecarp.cville_eda_derived.geopin` gpin
+  cross join `whatthecarp.cville_eda_raw.road_centerlines` roads
 )
 where rank = 1'
