d567a1a35ec6e2d3c8428b4079262c5067d7eb5c
Joshua Carp <jm.carp@gmail.com>
2021-04-22 00:20:19 -0400

    Analyze census demographics by zone.

---
+++ auto-zone/analyze-demographics-by-zone.sh
@@ -0,0 +1,76 @@
+#!/bin/bash
+
+set -euo pipefail
+
+bq query --nouse_legacy_sql \
+'create or replace function whatthecarp.cville_eda_derived.standardize_zone(zone string) returns string as (
+case zone
+  when "R-1UH" then "R-1U"
+  when "R-1H" then "R-1"
+  when "B-1H" then "B-1"
+  when "UHDH" then "UHD"
+  when "UMDH" then "UMD"
+  when "R-2C" then "R-2"
+  when "DNH" then "DN"
+  when "R-1SC" then "R-1S"
+  when "WSH" then "WS"
+  when "R-1SH" then "R-1S"
+  when "R-3H" then "R-3"
+  when "R-1C" then "R-1"
+  when "MLTPC" then "MLTP"
+  when "R-2UH" then "R-2U"
+  when "R-1SUH" then "R-1SU"
+  when "DNC" then "DN"
+  when "CDH" then "CD"
+  when "B-3H" then "B-3"
+  when "DH" then "D"
+  when "SSH" then "SS"
+  when "WMEH" then "WME"
+  when "R-2H" then "R-2"
+  when "CCH" then "CC"
+  when "WMWH" then "WMW"
+  when "B-1C" then "B-1"
+  when "CHH" then "CH"
+  when "R-1SHC" then "R-1S"
+  when "PUDH" then "PUD"
+  when "WMSH" then "WMS"
+  when "HSC" then "HS"
+  when "MLTPH" then "MLTP"
+  when "WSDH" then "WSD"
+  when "B-2H" then "B-2"
+  when "MRH" then "MR"
+  when "ICH" then "IC"
+  when "WMNH" then "WMN"
+  when "DEH" then "DE"
+  when "NCCH" then "NCC"
+  else zone
+end);'
+
+bq query --nouse_legacy_sql \
+'create or replace table `whatthecarp.cville_eda_derived.sf1_by_zoning` as
+select
+  whatthecarp.cville_eda_derived.standardize_zone(details.zoning) as zoning,
+  avg(safe_divide(P003003, P003001)) as prop_black,
+  avg(safe_divide(P003002, P003001)) as prop_white,
+  count(details.zoning) as parcels,
+from `whatthecarp.cville_eda_raw.parcel_area_details` details
+join `whatthecarp.cville_eda_derived.geopin_to_block` g2b on details.GeoParcelI = g2b.gpin
+join `whatthecarp.cville_eda_raw.sf1` sf1
+  on cast(g2b.geoid10 as string) =
+  concat(format("%02d", sf1.state), format("%03d", sf1.county), format("%06d", sf1.tract), format("%04d", sf1.block))
+group by zoning'
+
+bq query --nouse_legacy_sql \
+'create or replace table `whatthecarp.cville_eda_derived.acs_by_zoning` as
+select
+  whatthecarp.cville_eda_derived.standardize_zone(details.zoning) as zoning,
+  avg(safe_divide(B02001_003E, B02001_001E)) as prop_black,
+  avg(safe_divide(B02001_002E, B02001_001E)) as prop_white,
+  avg(if(B19013_001E >= 0, B19013_001E, null)) as income,
+  count(details.zoning) as parcels,
+from `whatthecarp.cville_eda_raw.parcel_area_details` details
+join `whatthecarp.cville_eda_derived.geopin_to_block` g2b on details.GeoParcelI = g2b.gpin
+join `whatthecarp.cville_eda_raw.acs_blockgroup` acs
+  on cast(floor(g2b.geoid10 / 1000) as string) =
+  concat(format("%02d", acs.state), format("%03d", acs.county), format("%06d", acs.tract), format("%01d", acs.block_group))
+group by zoning'
